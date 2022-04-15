a78a94d05dd249187213a2bb4fbdfeba0d009894
Seg <seg@haxxed.com>
2022-04-15 03:36:26 -0700

    refactor

--- portable/xorshift128.h
+++ portable/xorshift128.h
@@ -12,8 +12,8 @@ static uint32_t rng_xor128_u32(uint32_t* state){
 	return state[0] = t ^ s ^ (s >> 19);
 }
 
-uint32_t rng_xor128_state[4]={0xabb41ca8,0x468a5b62,0x89b11400,0x6cb6b5d9};
+uint32_t rng_xor128_state[4]={0x81300422,0x49680d02,0x827b0548,0xe9c907f0};
 
 SHL uint32_t rng_xor128(uint32_t n){
-	return (rng_xor128_u32(rng_xor128_state)-1)/((UINT32_MAX-1)/(n-1));
+	return rng_xor128_u32(rng_xor128_state)/(UINT32_MAX/n);
 }
