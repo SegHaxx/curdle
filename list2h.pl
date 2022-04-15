2b40b5655d8686306f8c8352a064e57f8cf5c417
Seg <seg@haxxed.com>
2022-04-15 01:38:46 -0700

    Flip wordlist array for better packing

--- list2h.pl
+++ list2h.pl
@@ -2,27 +2,26 @@
 use strict;
 use warnings;
 
-my @curdlist=map{uc$_}<STDIN>;
+my @wordlist=map{uc$_}<STDIN>;
 
-print "static int curdlist_count=",scalar @curdlist,";\n";
+print "static int curdlist_count=",scalar @wordlist,";\n";
 
-print "static unsigned char curdlist[]={\n";
-foreach(@curdlist){
-	chomp;
-	my $base26;
-	$base26 =ord(substr($_,0,1))-ord('A');
-	$base26*=26;
-	$base26+=ord(substr($_,1,1))-ord('A');
-	$base26*=26;
-	$base26+=ord(substr($_,2,1))-ord('A');
-	$base26*=26;
-	$base26+=ord(substr($_,3,1))-ord('A');
-	$base26*=26;
-	$base26+=ord(substr($_,4,1))-ord('A');
-	$base26=sprintf("%06x",$base26);
-	print "\t0x";
-	print substr($base26,0,2).",0x";
-	print substr($base26,2,2).",0x";
-	print substr($base26,4,2).",\n";
+foreach my $i (0,1,2){
+	print "static unsigned char curdlist$i\[]={";
+	foreach(@wordlist){
+		chomp;
+		my $base26;
+		$base26 =ord(substr($_,0,1))-ord('A');
+		$base26*=26;
+		$base26+=ord(substr($_,1,1))-ord('A');
+		$base26*=26;
+		$base26+=ord(substr($_,2,1))-ord('A');
+		$base26*=26;
+		$base26+=ord(substr($_,3,1))-ord('A');
+		$base26*=26;
+		$base26+=ord(substr($_,4,1))-ord('A');
+		$base26=sprintf("%06x",$base26);
+		print "0x".substr($base26,$i*2,2).",";
+	}
+	print "};\n";
 }
-print "};\n";
