e6deb849dcfaf1b91c6f44435bd2c7977be7faca
Seg <seg@haxxed.com>
2022-04-14 23:56:56 -0700

    base26 encoded wordlist

--- list2h.pl
+++ list2h.pl
@@ -2,13 +2,27 @@
 use strict;
 use warnings;
 
-my @wordlist=map{uc$_}<STDIN>;
+my @curdlist=map{uc$_}<STDIN>;
 
-print "static int wordlist_count=",scalar @wordlist,";\n";
+print "static int curdlist_count=",scalar @curdlist,";\n";
 
-print "static char* wordlist={\n";
-foreach(@wordlist){
+print "static unsigned char curdlist[]={\n";
+foreach(@curdlist){
 	chomp;
-	print qq(\t"$_"\n);
+	my $base26;
+	$base26 =ord(substr($_,0,1))-ord('A');
+	$base26*=26;
+	$base26+=ord(substr($_,1,1))-ord('A');
+	$base26*=26;
+	$base26+=ord(substr($_,2,1))-ord('A');
+	$base26*=26;
+	$base26+=ord(substr($_,3,1))-ord('A');
+	$base26*=26;
+	$base26+=ord(substr($_,4,1))-ord('A');
+	$base26=sprintf("%06x",$base26);
+	print "\t0x";
+	print substr($base26,0,2).",0x";
+	print substr($base26,2,2).",0x";
+	print substr($base26,4,2).",\n";
 }
 print "};\n";
